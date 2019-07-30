db9ba4bbed293ec3aeeaea55bdaff8a88f0c0525
Mangesh Suryawanshi <almtrainingcybage@gmail.com>
2019-07-30 18:57:38 +0530

    Push code to remote

---
+++ src/main/java/com/cybage/machineinfo/MachineInfoApplication.java
@@ -0,0 +1,13 @@
+package com.cybage.machineinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineInfoApplication.class, args);
+	}
+
+}
